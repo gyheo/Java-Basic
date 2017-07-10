d985e1521b56634d84bcbebeda698deb6c252148
KunYoung Heo <notegrapher@gmail.com>
2017-07-10 20:39:54 +0900

    MethodOverloading

---
+++ src/MethodOverloading.java
@@ -0,0 +1,18 @@
+
+public class MethodOverloading {
+	public int Add(int x, int y){
+		return x + y;
+	}
+
+	public int Add(int x, int y, int z){
+		return x + y + z;
+	}
+	
+	public String Add(String x, String y){
+		return x + y;
+	}
+	
+//	public int Add(int num1, int num2){
+//		
+//	}
+}
---
+++ src/MethodOverloadingExercise.java
@@ -0,0 +1,13 @@
+
+public class MethodOverloadingExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MethodOverloading mo = new MethodOverloading();
+		
+		System.out.println(mo.Add(10, 20));
+		System.out.println(mo.Add("Java ", "JSP"));
+		System.out.println(mo.Add(10, 20, 30));
+	}
+
+}
