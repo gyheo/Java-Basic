274726842c6b9c03ba77a0c6a7649c44239b3d9f
KunYoung Heo <notegrapher@gmail.com>
2017-07-10 15:35:17 +0900

    Reference Type

---
+++ src/ReferenceType.java
@@ -0,0 +1,13 @@
+
+public class ReferenceType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Basic Type
+		int i = 4;
+		
+		// Reference Type
+		String str = new String("Java");
+	}
+
+}
