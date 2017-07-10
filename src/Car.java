874618f37fdc836b21c8c419e79bd639cedd86b5
KunYoung Heo <notegrapher@gmail.com>
2017-07-10 20:29:46 +0900

    Constructor Exercise

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,12 @@
 public class Car {
 	String name;
 	int number;
+	
+	public Car(){
+		
+	}
+	
+	public Car(String name){
+		this.name = name;
+	}
 }
--- src/CarExercise.java
+++ src/CarExercise.java
@@ -4,6 +4,7 @@ public class CarExercise {
 		// Mulitple instance
 		Car c1 = new Car();
 		Car c2 = new Car();
+		Car c3 = new Car("경찰차");
 		
 		c1.name = "승용차";
 		c1.number = 1111;
@@ -16,5 +17,8 @@ public class CarExercise {
 	
 		System.out.println(c2.name);
 		System.out.println(c2.number);
+		
+		System.out.println(c3.name);
+		
 	}
 }
