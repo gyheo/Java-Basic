a5260fe9b502c111b4670f6e01e6f9e88a99b4e9
KunYoung Heo <notegrapher@gmail.com>
2017-07-10 15:23:09 +0900

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+
+public class Variables {
+	public static void main(String[] args){
+		boolean isFalse = false;
+		int num1 = 201640100;
+		// long PI = (long) 3.14L;
+		float PI2 = 3.141592F;
+		
+		System.out.println(isFalse);
+		System.out.println(num1);
+		System.out.println(PI2);
+	}
+}
