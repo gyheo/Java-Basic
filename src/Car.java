74ed5c7f313f2a2fbcac51d2312c0d2539535ade
KunYoung Heo <notegrapher@gmail.com>
2017-07-10 15:44:23 +0900

    field declare

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,5 @@
 
 public class Car {
-	
+	String name;
+	int number;
 }
--- src/CarExercise.java
+++ src/CarExercise.java
@@ -5,5 +5,16 @@ public class CarExercise {
 		Car c1 = new Car();
 		Car c2 = new Car();
 		
+		c1.name = "승용차";
+		c1.number = 1111;
+		
+		c2.name = "버스";
+		c2.number = 2222;
+		
+		System.out.println(c1.name);
+		System.out.println(c1.number);
+	
+		System.out.println(c2.name);
+		System.out.println(c2.number);
 	}
 }
