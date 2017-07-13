74fa7ff4f43a6476f3e626ed3e62d1cbc427e229
KunYoung Heo <notegrapher@gmail.com>
2017-07-13 21:03:39 +0900

    Accesser

---
+++ src/Accesser.java
@@ -0,0 +1,8 @@
+
+public class Accesser {
+	public int p1 = 3;
+	protected int p2 = 4;
+	int k = 2;
+	private int i = 10;
+	
+}
---
+++ src/com/seconds/javabasic/AccessExercise.java
@@ -0,0 +1,17 @@
+package com.seconds.javabasic;
+
+import JavaBasic.Accesser;
+
+public class AccessExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Accesser obj = new Accesser();
+		
+		System.out.println(obj.p1);
+		System.out.println(obj.p2);
+		System.out.println(obj.k);
+		System.out.println(obj.i);
+	}
+
+}
