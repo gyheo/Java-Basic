cd9ea0f770934f3e689601eec1ce9d2efb9cefc0
KunYoung Heo <notegrapher@gmail.com>
2017-07-19 17:09:22 +0900

    Math Class

---
+++ src/MathExercise.java
@@ -0,0 +1,18 @@
+
+public class MathExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = Math.max(5, 333);
+		System.out.println(value1);
+		
+		int value2 = Math.min(5, 333);
+		System.out.println(value2);
+		
+		System.out.println(Math.abs(-12312));
+		System.out.println(Math.random());
+		System.out.println(Math.sqrt(36));
+		
+	}
+
+}
