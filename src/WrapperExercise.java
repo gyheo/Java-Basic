bac3bd6b2d541a77f8e4680b5da2fe2e147fe2ea
KunYoung Heo <notegrapher@gmail.com>
2017-07-19 14:55:34 +0900

    Auto Boxing & Unboxing

---
+++ src/WrapperExercise.java
@@ -0,0 +1,18 @@
+
+public class WrapperExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 5;
+		Integer i2 = new Integer(5);
+		
+		// Auto Incast
+		// Integer i2 = new Integer(5);
+		Integer i3 = 5;
+		
+		int i4 = i3.intValue();
+		// Auto Unboxing
+		int i5 = i3;
+	}
+
+}
