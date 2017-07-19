7bfbb73adeec2cbfd4f477d0367150e8b6b6724a
KunYoung Heo <notegrapher@gmail.com>
2017-07-19 18:29:22 +0900

    Date & Calendar

---
+++ src/CalendarExercise.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+
+public class CalendarExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal.get(Calendar.YEAR));
+		System.out.println(cal.get(Calendar.MONTH) + 1);
+		System.out.println(cal.get(Calendar.DATE));
+
+		System.out.println(cal.get(Calendar.HOUR));
+		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+		System.out.println(cal.get(Calendar.MINUTE));
+		
+		cal.add(Calendar.HOUR, 5);
+		
+		System.out.println(cal.get(Calendar.YEAR));
+		System.out.println(cal.get(Calendar.MONTH) + 1);
+		System.out.println(cal.get(Calendar.DATE));
+
+		System.out.println(cal.get(Calendar.HOUR));
+		System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+		System.out.println(cal.get(Calendar.MINUTE));
+
+	}
+
+}
